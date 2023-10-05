e2bf62ada9bc0d94b3dd96dd8c89fefb1678ab12
Camilo Garcia <37909560+formala@users.noreply.github.com>
2023-10-04 22:00:53 -0500

    Delete prueba_odoo/technical_tests directory

--- prueba_odoo/technical_tests/static/src/js/models.js
+++
@@ -1,9 +0,0 @@
-odoo.define('technical_test.Models', function (require) {
-  "use strict";
-
-  var models = require('point_of_sale.models');
-  var core = require('web.core');
-
-  models.load_fields("res_partner", ["age"]);
-
-});
